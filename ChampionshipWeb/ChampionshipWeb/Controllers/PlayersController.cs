0120c5a35a9a5d6cf468ab888d4b0059236b408d
AlexandrPro <Alexandr.Prokofiev.A@gmail.com>
2016-05-04 12:37:36 +0300

    First work version

---
+++ ChampionshipWeb/ChampionshipWeb/Controllers/PlayersController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ChampionshipWeb.Models;
+
+namespace ChampionshipWeb.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private championshipEntities db = new championshipEntities();
+
+        // GET: Players
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            var players = db.players.Include(p => p.player_state).Include(p => p.position).Include(p => p.team);
+            return View(players.ToList());
+        }
+
+        // GET: Players/Details/5
+        [AllowAnonymous]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            player player = db.players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // GET: Players/Create
+        [Authorize(Roles = "admin, moder")]
+        public ActionResult Create()
+        {
+            ViewBag.player_state_id = new SelectList(db.player_state, "id", "name_ru");
+            ViewBag.position_id = new SelectList(db.positions, "id", "name_ru");
+            ViewBag.team_id = new SelectList(db.teams, "id", "short_name");
+            return View();
+        }
+
+        // POST: Players/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [Authorize(Roles = "admin, moder")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,name,team_id,date_of_birth,weight,height,position_id,photo_path,player_state_id")] player player)
+        {
+            if (ModelState.IsValid)
+            {
+                db.players.Add(player);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.player_state_id = new SelectList(db.player_state, "id", "name_ru", player.player_state_id);
+            ViewBag.position_id = new SelectList(db.positions, "id", "name_ru", player.position_id);
+            ViewBag.team_id = new SelectList(db.teams, "id", "short_name", player.team_id);
+            return View(player);
+        }
+
+        // GET: Players/Edit/5
+        [Authorize(Roles = "admin, moder")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            player player = db.players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.player_state_id = new SelectList(db.player_state, "id", "name_ru", player.player_state_id);
+            ViewBag.position_id = new SelectList(db.positions, "id", "name_ru", player.position_id);
+            ViewBag.team_id = new SelectList(db.teams, "id", "short_name", player.team_id);
+            return View(player);
+        }
+
+        // POST: Players/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [Authorize(Roles = "admin, moder")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name,team_id,date_of_birth,weight,height,position_id,photo_path,player_state_id")] player player)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(player).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.player_state_id = new SelectList(db.player_state, "id", "name_ru", player.player_state_id);
+            ViewBag.position_id = new SelectList(db.positions, "id", "name_ru", player.position_id);
+            ViewBag.team_id = new SelectList(db.teams, "id", "short_name", player.team_id);
+            return View(player);
+        }
+
+        // GET: Players/Delete/5
+        [Authorize(Roles = "admin, moder")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            player player = db.players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // POST: Players/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [Authorize(Roles = "admin, moder")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            player player = db.players.Find(id);
+            db.players.Remove(player);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ChampionshipWeb/ChampionshipWeb/Providers/MyRoleProvider.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.Helpers;
+using System.Web.WebPages;
+using Microsoft.Internal.Web.Utils;
+using ChampionshipWeb.Models;
+
+namespace ChampionshipWeb.Providers
+{
+    public class MyRoleProvider : RoleProvider
+    {
+        public override string[] GetRolesForUser(string login)
+        {
+            string[] role = new string[] { };
+            using (championshipEntities _db = new championshipEntities())
+            {
+                try
+                {
+                    // Получаем пользователя
+                    user user_ = (from u in _db.users
+                                 where u.login == login
+                                 select u).FirstOrDefault();
+                    if (user_ != null)
+                    {
+                        // получаем роль
+                        role userRole = _db.roles.Find(user_.role);
+
+                        if (userRole != null)
+                        {
+                            role = new string[] { userRole.name };
+                        }
+                    }
+                }
+                catch
+                {
+                    role = new string[] { };
+                }
+            }
+            return role;
+        }
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            bool outputResult = false;
+            // Находим пользователя
+            using (championshipEntities _db = new championshipEntities())
+            {
+                try
+                {
+                    // Получаем пользователя
+                    user user_ = (from u in _db.users
+                                 where u.login == username
+                                 select u).FirstOrDefault();
+                    if (user_ != null)
+                    {
+                        // получаем роль
+                        role userRole = _db.roles.Find(user_.role);
+
+                        //сравниваем
+                        if (userRole != null && userRole.name == roleName)
+                        {
+                            outputResult = true;
+                        }
+                    }
+                }
+                catch
+                {
+                    outputResult = false;
+                }
+            }
+            return outputResult;
+        }
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
